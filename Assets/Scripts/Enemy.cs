a066b100554981aa3542db010b3cba775b98e8db
Local Pizza Employee <LocalPizzaEmployee@users.noreply.github.com>
2023-07-10 16:41:20 +0800

    Minor Changes - Fixed some lines in Enemy Script - Created player and enemy prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -35,31 +35,31 @@ public class Enemy : MonoBehaviour
 
         //Find a way to simplfy this and also, figure out how to insert a code that will play a chase music without it reapeating everytime the enemy keeps finding the player
         //You can use a boolean to detect whether or not the player is heard and will only play the music once and then goes back off once the distance from the player is far enough
+
         hearDistance = Vector3.Distance(transform.position, target.transform.position);
         if (target.GetComponent<FirstPersonController>().CurrentInput.x > 0.1f && hearDistance < hearRadiusWalk || target.GetComponent<FirstPersonController>().CurrentInput.x < -0.1f && hearDistance < hearRadiusWalk)
         {
-            //the sequence for the if statement could be turned into a coroutine if you want, or a function by itself like private void playerHeard()
-            navMeshAgent.SetDestination(target.position);
+            playerHeard();
             Debug.Log("I CAN HEAR YOU WALKING");      
         }
-        else if (target.GetComponent<FirstPersonController>().CurrentInput.x > 3.5f && hearDistance < hearRadiusRun || target.GetComponent<FirstPersonController>().CurrentInput.x < -3.5f && hearDistance < hearRadiusWalk)
+        else if (target.GetComponent<FirstPersonController>().CurrentInput.x > 3.5f && hearDistance < hearRadiusRun || target.GetComponent<FirstPersonController>().CurrentInput.x < -3.5f && hearDistance < hearRadiusRun)
         {
-            navMeshAgent.SetDestination(target.position);
+            playerHeard();
             Debug.Log("I CAN HEAR YOU RUNNING");
         }
         else if (target.GetComponent<FirstPersonController>().CurrentInput.y > 0.1f && hearDistance < hearRadiusWalk || target.GetComponent<FirstPersonController>().CurrentInput.y < -0.1f && hearDistance < hearRadiusWalk)
         {
-            navMeshAgent.SetDestination(target.position);
+            playerHeard();
             Debug.Log("I CAN HEAR YOU WALKING");
         }
         else if (target.GetComponent<FirstPersonController>().CurrentInput.y > 3.5f && hearDistance < hearRadiusRun || target.GetComponent<FirstPersonController>().CurrentInput.y < -3.5f && hearDistance < hearRadiusRun)
         {
-            navMeshAgent.SetDestination(target.position);
+            playerHeard();
             Debug.Log("I CAN HEAR YOU RUNNING");
         }
         else if (hearDistance < hearRadius)
         {
-            navMeshAgent.SetDestination(target.position);
+            playerHeard();
             Debug.Log("I CAN HEAR BREATHING");
         }
 
@@ -85,14 +85,8 @@ public class Enemy : MonoBehaviour
         result = Vector3.zero;
         return false;
     }
-
-    /*
-     * Would be great to use this instead of repeating the same code for the if statements
-     * Figure out how to make the enemy speed up using navmesh.speed = 5 or 6 whenever the enemy detects the player and then go back to 3 when they are no longer detected
-     * 
     private void playerHeard()
     {
         navMeshAgent.SetDestination(target.position);
     }
-    */
 }
